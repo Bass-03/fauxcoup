078d2a932ed902b18f9b0bd2ec815425024056fe
Edmundo Sanchez <zomundo@gmail.com>
2018-01-15 03:08:10 -0600

    pelican config update

--- pelicanconf.py
+++ pelicanconf.py
@@ -7,10 +7,39 @@ SITENAME = 'fauxcoup'
 SITEURL = ''
 
 PATH = 'content'
+USE_FOLDER_AS_CATEGORY = False
 
 TIMEZONE = 'America/Mexico_City'
 
-DEFAULT_LANG = 'en'
+#My settings
+DISPLAY_PAGES_ON_MENU = True
+DISPLAY_CATEGORIES_ON_MENU = True
+SITESUBTITLE = "Aprende conmigo"
+GITHUB_URL = "https://github.com/gdledsan/fauxcoup"
+DEFAULT_DATE = 'fs'
+PLUGIN_PATHS = ["plugins"]
+PLUGINS = ['i18n_subsites']
+#JINJA_EXTENSIONS = ['jinja2.ext.i18n']
+
+# mapping: language_code -> settings_overrides_dict
+I18N_SUBSITES = {
+    'en': {
+        'SITENAME': 'FauxCoup',
+        'THEME':'themes/nmi'
+        },
+    'es': {
+        'SITENAME': 'FauxCoup',
+        'THEME':'themes/nmi.ES'
+        },
+    'fr': {
+        'SITENAME': 'FauxCoup',
+        'THEME':'themes/nmi.FR'
+        }
+    }
+
+#themes
+THEME_STATIC_DIR = 'themes'
+THEME = 'themes/nmi'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
